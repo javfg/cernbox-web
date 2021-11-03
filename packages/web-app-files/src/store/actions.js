1196da7484f235babd2dccb86e1c3b6e2d0166be
Diogo Castro <diogo.castro@cern.ch>
2021-11-03 20:55:52 +0100

    CERN ONLY do not wait for share info to load

--- packages/web-app-files/src/store/actions.js
+++ packages/web-app-files/src/store/actions.js
@@ -494,12 +494,13 @@ export default {
     commit('CLEAR_RESOURCES_TO_DELETE_LIST')
   },
 
-  async loadIndicators({ dispatch, commit }, { client, currentFolder }) {
+  loadIndicators({ dispatch, commit }, { client, currentFolder }) {
     // kind of bruteforce for now: remove the shares for the current folder and children, reload shares tree for the current folder.
     // TODO: when we refactor the shares tree we want to modify shares tree nodes incrementally during adding and removing shares, not loading everything new from the backend.
     commit('SHARESTREE_PRUNE_OUTSIDE_PATH', dirname(currentFolder))
-    await dispatch('loadSharesTree', { client, path: currentFolder })
-    commit('LOAD_INDICATORS')
+    const promiseLoad = dispatch('loadSharesTree', { client, path: currentFolder })
+    promiseLoad.then(commit('LOAD_INDICATORS'))
+    return promiseLoad
   },
 
   loadAvatars({ commit, rootGetters }, { resource }) {
