70793052a4cc03b1c79384d486b192df69570599
Diogo Castro <diogo.castro@cern.ch>
2021-12-02 18:31:43 +0100

    EOS Projects

---
+++ packages/web-app-files/src/mixins/actions/openTrashbin.js
@@ -0,0 +1,37 @@
+import { isProjectsRoute } from '../../helpers/route'
+
+export default {
+  computed: {
+    $_open_trashbin() {
+      return [
+        {
+          icon: 'delete',
+          handler: (resource) => this.$_navigate_to_trashbin(resource),
+          label: () =>
+            this.$pgettext(
+              'Action in the files list row to go to trashbin of selected project',
+              'Open trashbin'
+            ),
+          isEnabled: ({ resource }) => {
+            console.log('wihich route', isProjectsRoute)
+            if (!isProjectsRoute(this.$route)) {
+              return false
+            }
+
+            return true
+          },
+          componentType: 'oc-button',
+          class: 'oc-files-actions-accept-share-trigger'
+        }
+      ]
+    }
+  },
+  methods: {
+    $_navigate_to_trashbin(resource) {
+      this.$router.push({
+        path: '/files/list/trash-bin-project',
+        query: { project: resource.path, name: resource.name }
+      })
+    }
+  }
+}
