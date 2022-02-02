2f0f0de18e30dcdb2ed1fa685206e36f86f67d51
elizavetaRa <svillyfly@mail.ru>
2022-02-02 01:15:34 +0100

    CERNONLY Feedback link

--- packages/web-runtime/src/components/Topbar/FeedbackLink.vue
+++ packages/web-runtime/src/components/Topbar/FeedbackLink.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="oc-flex">
     <oc-button
+      id="cernbox-feedback-button"
       v-oc-tooltip="description"
       type="a"
       :href="href"
@@ -21,14 +22,14 @@ export default {
   name: 'FeedbackLink',
   computed: {
     href() {
-      return 'https://owncloud.com/web-design-feedback'
+      return 'https://codimd.web.cern.ch/TKGx1_6nQMK2OEHfmQ4XUQ'
     },
     ariaLabel() {
-      return this.$gettext('ownCloud feedback survey')
+      return this.$gettext('Give Us Feedback')
     },
     description() {
       return this.$gettext(
-        "Provide your feedback: We'd like to improve the web design and would be happy to hear your feedback. Thank you! Your ownCloud team."
+        'This version is still in development and your feedback is very important to us! Please add your comments in the following document.'
       )
     }
   }
