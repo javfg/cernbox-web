cernbox/web

App name in title of external app tab

packages/web-app-external/src/App.vue
@@ -88,6 +88,9 @@ export default {
       return this.$route.params.file_id
     }
   },
+  mounted() {
+    document.title = this.$route.params.app
+  },
   async created() {
     await unauthenticatedUserReady(this.$router, this.$store)
 

javfg/cernbox-web

packages/web-app-external/src/App.vue
@@ -88,6 +88,9 @@ export default {
       return this.$route.params.fileId
     }
   },
+  mounted() {
+    document.title = this.$route.params.appName
+  },
   async created() {
     await unauthenticatedUserReady(this.$router, this.$store)
 