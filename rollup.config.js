961aa6c76739b906310214e992a2ae17ae42f92d
elizavetaRa <svillyfly@mail.ru>
2022-04-27 11:11:52 +0200

    Revert "CERNONLY enable router-history-mode"
    This reverts commit 478704f49daf0a6e359ff98c84434794cde22d38.

--- rollup.config.js
+++ rollup.config.js
@@ -29,9 +29,7 @@ const compilationTimestamp = new Date().getTime()
 
 const config = {
   requirejs: {},
-  cdn: process.env.CDN === 'true',
-  baseUrl: process.env.BASE_URL || '/',
-  history_mode: process.env.HISTORY_MODE === 'true'
+  cdn: process.env.CDN === 'true'
 }
 if (process.env.REQUIRE_TIMEOUT) {
   config.requirejs.waitSeconds = parseInt(process.env.REQUIRE_TIMEOUT)
@@ -159,8 +157,8 @@ const plugins = [
                 return acc
               }, {}),
               roots: {
-                css: config.baseUrl + 'css',
-                js: config.baseUrl + 'js'
+                css: 'css',
+                js: 'js'
               },
               config: config,
               compilationTimestamp: compilationTimestamp
