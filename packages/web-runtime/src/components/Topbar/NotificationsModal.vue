27bdd6a475f9bbd95479b2a2715ce4ffa9be6d05
Daniel Sparemblek <danielrey1@gmail.com>
2023-08-23 18:13:48 +0200

    Lint new notification modal

--- packages/web-runtime/src/components/Topbar/NotificationsModal.vue
+++ packages/web-runtime/src/components/Topbar/NotificationsModal.vue
@@ -1,54 +1,46 @@
 <template>
-    <oc-modal
-      :title="$gettext('Notification Settings')"
-      :button-cancel-text="$gettext('Discard')"
-      :button-confirm-text="$gettext('Save')"
-      @cancel="$emit('cancel')"
-      @confirm="onConfirm"
-    >
-      <template #content>
-        <oc-checkbox 
-          v-model="localNotifyEnabled" 
-          label="Disable all notifications" 
-        />
-      </template>
-    </oc-modal>
-  </template>
+  <oc-modal :title="$gettext('Notification Settings')" :button-cancel-text="$gettext('Discard')"
+    :button-confirm-text="$gettext('Save')" @cancel="$emit('cancel')" @confirm="onConfirm">
+    <template #content>
+      <oc-checkbox v-model="localNotifyEnabled" label="Disable all notifications" />
+    </template>
+  </oc-modal>
+</template>
   
-  <script lang="ts">
-  import { defineComponent, ref, watch, PropType } from 'vue'
-  import { useGraphClient } from 'web-pkg'
-  
-  export default defineComponent({
-    name: 'NotificationsModal',
-    emits: ['cancel', 'update:initialNotifyEnabled', 'confirm'],
-    props: {
-      initialNotifyEnabled: {
-        type: Boolean as PropType<boolean>,
-        required: true
-      },
+<script lang="ts">
+import { defineComponent, ref, watch, PropType } from 'vue'
+import { useGraphClient } from 'web-pkg'
+
+export default defineComponent({
+  name: 'NotificationsModal',
+  emits: ['cancel', 'update:initialNotifyEnabled', 'confirm'],
+  props: {
+    initialNotifyEnabled: {
+      type: Boolean as PropType<boolean>,
+      required: true
     },
-    setup(props, { emit }) {
-      const localNotifyEnabled = ref(props.initialNotifyEnabled);
+  },
+  setup(props, { emit }) {
+    const localNotifyEnabled = ref(props.initialNotifyEnabled);
 
-      watch(
-        () => props.initialNotifyEnabled,
-        (newVal) => {
-          localNotifyEnabled.value = newVal;
-        }
-      );
+    watch(
+      () => props.initialNotifyEnabled,
+      (newVal) => {
+        localNotifyEnabled.value = newVal;
+      }
+    );
 
-      const onConfirm = () => {
-        emit('update:initialNotifyEnabled', localNotifyEnabled.value);
-        emit('confirm', localNotifyEnabled.value);
-      };
+    const onConfirm = () => {
+      emit('update:initialNotifyEnabled', localNotifyEnabled.value);
+      emit('confirm', localNotifyEnabled.value);
+    };
 
-      return {
-        localNotifyEnabled,
-        onConfirm,
-        ...useGraphClient()
-      };
-    }
-  })
-  </script>
+    return {
+      localNotifyEnabled,
+      onConfirm,
+      ...useGraphClient()
+    };
+  }
+})
+</script>
   
